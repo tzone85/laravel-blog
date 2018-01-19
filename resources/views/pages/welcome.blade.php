ac0acc3e6edc5bcf2a20ab336d1f121833178434
thando <tzone85@gmail.com>
2018-01-19 14:08:29 +0200

    added cdm bootstrap for css and js. did static pages using html for the three pages. Still to replace them with the blade templating engine

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,96 +1,118 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Laravel Blog</title>
 
-        <title>Laravel</title>
+    <!-- Bootstrap -->
+    {{--<link href="css/bootstrap.min.css" rel="stylesheet">--}}
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
 
-            .full-height {
-                height: 100vh;
-            }
+<!-- Default Bootstrap Navbar  -->
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Laravel Blog</a>
+        </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="/">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
 
-            .position-ref {
-                position: relative;
-            }
+            </ul>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+            <ul class="nav navbar-nav navbar-right">
 
-            .content {
-                text-align: center;
-            }
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Welcome Everyone!
-                </div>
-
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="jumbotron">
+                <h1>Welcome to my blog</h1>
+                <p class="lead">Thank you for visiting. This is my test website with Laravel. Please read my polular post!  </p>
+                <p><a href="#" class="btn btn-primary btn-lg" role="button">Popular Post</a></p>
             </div>
         </div>
-    </body>
+    </div> <!-- end of .row  -->
+
+    <div class="row">
+        <div class="col-md-8">
+            <div class="post">
+                <h3>Post Title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, dolorum ea iusto maiores nam reiciendis repellendus rerum unde voluptatem. Cupiditate dolor id molestias neque, pariatur praesentium quia quos reiciendis voluptas!...</p>
+                <a href="#" class="btn btn-primary">Read More</a>
+            </div>
+            <hr>
+            <div class="post">
+                <h3>Post Title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, dolorum ea iusto maiores nam reiciendis repellendus rerum unde voluptatem. Cupiditate dolor id molestias neque, pariatur praesentium quia quos reiciendis voluptas!...</p>
+                <a href="#" class="btn btn-primary">Read More</a>
+            </div>
+            <hr>
+            <div class="post">
+                <h3>Post Title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, dolorum ea iusto maiores nam reiciendis repellendus rerum unde voluptatem. Cupiditate dolor id molestias neque, pariatur praesentium quia quos reiciendis voluptas!...</p>
+                <a href="#" class="btn btn-primary">Read More</a>
+            </div>
+            <hr>
+            <div class="post">
+                <h3>Post Title</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, dolorum ea iusto maiores nam reiciendis repellendus rerum unde voluptatem. Cupiditate dolor id molestias neque, pariatur praesentium quia quos reiciendis voluptas!...</p>
+                <a href="#" class="btn btn-primary">Read More</a>
+            </div>
+        </div>
+
+        <div class="col-md3 col-md-offset-1">
+            <h2>Sidebar</h2>
+
+        </div>
+    </div>
+
+</div> <!-- end of .container  -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+
+{{--<script src="js/bootstrap.min.js"></script>--}}
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
 </html>
