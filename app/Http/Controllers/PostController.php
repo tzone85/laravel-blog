85f0c93047d623c85ff168a19fb7a7a80d22ddd2
thando <tzone85@gmail.com>
2018-01-26 17:41:56 +0200

    added validation when creating posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
@@ -34,7 +35,26 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate the data
+
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ));
+
+        // store in db
+        $post = new Post;
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        $post->save();
+
+        // redirect to another page
+
+        return redirect()->route('posts.show', $post->id);
+
+
     }
 
     /**
