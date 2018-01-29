a9b446a8303935004dc471d2861f3be4c80665e1
thando <tzone85@gmail.com>
2018-01-29 20:06:13 +0200

    enhanced the posts.show page by getting dates from the database and formatting them accordingly.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,41 @@
 @section('title', '| View Post')
 
 @section('content')
-    <h1>{{ $post->title }}</h1>
-    <p class="lead">{{ $post->body }}</p>
+
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+            <p class="lead">{{ $post->body }}</p>
+
+        </div>
+
+
+
+        <div class="col-md-4">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Created At:</dt>
+                    <dd>{{ date('M j, Y H:i', strtotime($post->created_at)) }}</dd>
+                </dl>
+
+                <dl class="dl-horizontal">
+                    <dt>Last Updated:</dt>
+                    <dd>{{ date('M j, Y H:i', strtotime($post->updated_at)) }}</dd>
+                </dl>
+
+                <hr>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="#" class="btn btn-primary btn-block">Edit</a>
+                    </div>
+                    <div class="col-sm-6">
+                        <a href="#" class="btn btn-danger btn-block">Delete</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
 @endsection
